bfbe7a88c08d8c9672bc3e8c4767ef82e1096e6e
spprid <spprid@gmail.com>
2024-10-30 08:57:32 +0900

    테스트

---
+++ gomountine/View/MountineView.swift
@@ -0,0 +1,21 @@
+//
+//  MountineView.swift
+//  gomountine
+//
+//  Created by 향택 on 10/30/24.
+//
+
+import SwiftUI
+
+struct MountineView: View {
+    var body: some View {
+        Rectangle()
+            .fill(Color.blue)
+            .ignoresSafeArea()
+            
+    }
+}
+
+#Preview {
+    MountineView()
+}
